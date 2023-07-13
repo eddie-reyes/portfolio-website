1a54b404418c73cbdf93f99f869da417e49bebfb
eddie-reyes <eddie.kingsreyes@gmail.com>
2023-07-13 16:05:10 -0700

    added project card

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,57 +1,40 @@
-import React, { useEffect, useRef } from "react";
-import mundos from "../images/mundos.gif";
-import { Tooltip } from "bootstrap";
+import mundos from '../images/mundos.gif';
+import trendplots from '../images/trendplots.png';
+import ProjectCard from './ProjectCard';
 
 const Projects = () => {
-  const toolTipRef = useRef<HTMLButtonElement>(null);
+    return (
+        <section className="section" id="projects-section">
+            <h1 className="text-secondary">&lt;Projects/&gt;</h1>
+            <div className="position-absolute text-secondary center-line"></div>
 
-  useEffect(() => {
-    const tooltip = new Tooltip(toolTipRef.current as Element, {
-      title:
-        "Please wait for the AWS server to boot up, then refresh the page.",
-      placement: "bottom",
-      trigger: "hover",
-    });
-  });
+            <div className="d-flex flex-column align-items-center text-center text-white projects-layout">
+                <h1>2021</h1>
+                <h1>2022</h1>
+                <h1>2023</h1>
+                <h1>2024</h1>
+                <h1 className="mb-0">2025</h1>
 
-  return (
-    <section className="section" id="projects-section">
-      <h1 className="text-secondary">&lt;Projects/&gt;</h1>
-      <div className="position-absolute text-secondary center-line"></div>
-
-      <div className="d-flex flex-column align-items-center text-center text-white projects-layout">
-        <h1>2021</h1>
-        <h1>2022</h1>
-        <h1>2023</h1>
-        <h1>2024</h1>
-        <h1 className="mb-0">2025</h1>
-        <div className="d-flex flex-column align-items-center w-100 h-100 position-absolute project-cards">
-          <div className="card text-white bg-dark mb-3 project-1">
-            <h2 className="card-header bg-gradient">mundos.io - 2022</h2>
-            <div className="card-body">
-              <img className=" w-50 mb-3 rounded" src={mundos} alt="Mundos" />
-              <p className="card-text">
-                An online multiplayer game inspired by Katamari Damacy. Roll
-                around and collect mass in order to become the champion.
-              </p>
-              <button
-                type="button"
-                className="btn btn-primary"
-                ref={toolTipRef}
-              >
-                <a
-                  className="link-light text-decoration-none"
-                  href="http://mundos.io"
-                >
-                  Play Now
-                </a>
-              </button>
+                <div className="d-flex flex-column align-items-center w-100 h-100 position-absolute project-cards">
+                    <ProjectCard
+                        header="mundos.io - 2022"
+                        image={mundos}
+                        description="An online multiplayer game inspired by Katamari Damacy. Roll around and
+                        collect mass in order to become the champion."
+                        hyperlink="http://mundos.io"
+                        toolTipDescription="Please wait for the AWS server to boot up, then refresh the page."
+                    />
+                    <ProjectCard
+                        header="trendplots.com - 2023"
+                        image={trendplots}
+                        description="A real-time graphical visualization of global trend data from trends.google.com"
+                        hyperlink="https://trendplots.com"
+                        toolTipDescription=""
+                    />
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Projects;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,32 +1,31 @@
-import React from "react";
+import React from 'react';
 
 const Skills = () => {
-  return (
-    <section className="section">
-      <h1 className="text-secondary">&lt;Skills/&gt;</h1>
+    return (
+        <section className="section">
+            <h1 className="text-secondary">&lt;Skills/&gt;</h1>
 
-      <div className="d-flex flex-column text-white descriptor">
-        <div className="skills-section">
-          <h2 className="mb-3">&gt; Web</h2>
-          <li className="ps-5 mb-2">
-            Front-end: Javascript, Typescript, CSS, HTML, React, Bootstrap
-          </li>
-          <li className="ps-5 ">
-            Back-end: PostgreSQL, GraphQL, Apollo, Node.js, TypeORM, Redis,
-            Next.js
-          </li>
-        </div>
-        <div className="skills-section">
-          <h2 className="mb-3">&gt; Game Development</h2>
-          <li className="ps-5 ">Unreal Engine, Unity, GameMaker</li>
-        </div>
-        <div className="skills-section">
-          <h2 className="mb-3">&gt; General Purpose</h2>
-          <li className="ps-5 ">Python, C, C++, C#, Java</li>
-        </div>
-      </div>
-    </section>
-  );
+            <div className="d-flex flex-column text-white descriptor">
+                <div className="skills-section">
+                    <h2 className="mb-3">&gt; Web</h2>
+                    <li className="ps-5 mb-2">
+                        Front-end: Javascript, Typescript, CSS, HTML, React, Redux, Bootstrap
+                    </li>
+                    <li className="ps-5 ">
+                        Back-end: Node.js, Express, TypeORM, PostgresQL, Amazon Web Services
+                    </li>
+                </div>
+                <div className="skills-section">
+                    <h2 className="mb-3">&gt; Game Development</h2>
+                    <li className="ps-5 ">Unity, Unreal Engine, GameMaker</li>
+                </div>
+                <div className="skills-section">
+                    <h2 className="mb-3">&gt; General Purpose</h2>
+                    <li className="ps-5 ">Python, C, C++, C#, Java</li>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Skills;
